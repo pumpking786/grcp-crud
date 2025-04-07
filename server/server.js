9fd7dc700e02c6d2cddfead3122f547692d5274c
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-04-07 15:04:41 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
-const grpc = require("@grpc/grpc-js");
-const protoLoader = require("@grpc/proto-loader");
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
 // Load the proto file
-const PROTO_PATH = "../customers.proto";
+const PROTO_PATH = '../customers.proto';
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {});
 const proto = grpc.loadPackageDefinition(packageDefinition).CustomerService;
 
 // In-memory "database" for storing customers
 let customers = [
-  { id: "1", name: "John Doe", age: 30, address: "1234 Elm St" },
-  { id: "2", name: "Jane Smith", age: 25, address: "5678 Oak St" },
+  { id: '1', name: 'John Doe', age: 30, address: '1234 Elm St' },
+  { id: '2', name: 'Jane Smith', age: 25, address: '5678 Oak St' }
 ];
-
-// Sequential ID tracker
-let nextId = 3; // Start from 3 because IDs 1 and 2 are already used
 
 // Service methods
 const server = new grpc.Server();
+
+let nextId=3;
 
 // getAll method
 function getAll(call, callback) {
@@ -25,45 +24,45 @@ function getAll(call, callback) {
 
 // get method
 function get(call, callback) {
-  const customer = customers.find((c) => c.id === call.request.id);
+  const customer = customers.find(c => c.id === call.request.id);
   if (customer) {
     callback(null, customer);
   } else {
     callback({
       code: grpc.status.NOT_FOUND,
-      details: "Customer not found",
+      details: 'Customer not found'
     });
   }
 }
 
 // insert method
 function insert(call, callback) {
-  const newCustomer = { ...call.request, id: nextId.toString() };
+  const newCustomer = { ...call.request, id: nextId.toString()};
   customers.push(newCustomer);
-  nextId++; // Increment the ID for the next customer
+  nextId++;
   callback(null, newCustomer);
 }
 
 // update method
 function update(call, callback) {
-  const index = customers.findIndex((c) => c.id === call.request.id);
+  const index = customers.findIndex(c => c.id === call.request.id);
   if (index !== -1) {
     customers[index] = call.request;
     callback(null, customers[index]);
   } else {
     callback({
       code: grpc.status.NOT_FOUND,
-      details: "Customer not found",
+      details: 'Customer not found'
     });
   }
 }
 
-// remove method
 function remove(call, callback) {
   const index = customers.findIndex((c) => c.id === call.request.id);
   if (index !== -1) {
-    customers.splice(index, 1);
-    callback(null, {});
+    const deletedCustomer = customers.splice(index, 1)[0]; // Get the deleted customer
+    console.log(`Deleted Customer ID: ${deletedCustomer.id}`); // Add this line to debug
+    callback(null, { id: deletedCustomer.id });  // Ensure we're sending the id
   } else {
     callback({
       code: grpc.status.NOT_FOUND,
@@ -71,6 +70,9 @@ function remove(call, callback) {
     });
   }
 }
+
+
+
 
 // Add service to server
 server.addService(proto.service, {
@@ -78,15 +80,11 @@ server.addService(proto.service, {
   get,
   insert,
   update,
-  remove,
+  remove
 });
 
 // Start the server
-server.bindAsync(
-  "127.0.0.1:50051",
-  grpc.ServerCredentials.createInsecure(),
-  () => {
-    console.log("Server running at http://127.0.0.1:50051");
-    server.start();
-  }
-);
+server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), () => {
+  console.log('Server running at http://127.0.0.1:50051');
+  server.start();
+});
